6a97e73456577b954698a6085f2561f51114d027
Huong Nguyen <tienhuong.nguyen@ved.com.vn>
2023-08-23 10:50:42 +0700

    prob 168

--- lib/lib.h
+++ lib/lib.h
@@ -6,8 +6,24 @@
 
 #ifndef LC_LIB_H
 #define LC_LIB_H
-template <typename T> void printVector(std::vector<T> &v);
-template <typename T> void printMatrix(std::vector<std::vector<T>> &m);
+
+template<typename T>
+void printVector(std::vector<T> &v);
+
+template<typename T>
+void printMatrix(std::vector<std::vector<T>> &m);
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 #include "lib.cpp"
 
 #endif //LC_LIB_H
--- lib/stdlib.h
+++ lib/stdlib.h
@@ -12,6 +12,7 @@
 #include "unordered_map"
 #include "set"
 #include "unordered_set"
+#include "sstream"
 
 using namespace std;
 
